c45a040ec2939b1785d75b72bdacafaf54febd90
Charlie Best <79665492+CharlieBest@users.noreply.github.com>
2022-11-25 16:16:52 +0000

    Add files via upload

---
+++ GPLeaderboard.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+namespace Avoidance
+{
+    /// <summary>
+    /// Responsible for posting a score to the Google Play leaderboard and showing the leaderboard UI.
+    /// </summary>
+    public class GPLeaderboard : Singleton<GPLeaderboard>
+    {
+        /// <summary>
+        /// Posts the score if on Android platform.
+        /// </summary>
+        /// <param name="score">Score.</param>
+        public void PostScore(int score)
+        {
+            Debug.Log("Add code to post score to leaderboard here");
+        }
+
+        /// <summary>
+        /// Shows the Android leaderboard UI.
+        /// </summary>
+        public void ShowUI()
+        {
+            Debug.Log("Add code to show leaderboard here");
+        }
+    }
+}
